ea8322081d2531613cc88d61239c4065169d30db
David EPELY <65737+iamdey@users.noreply.github.com>
2026-07-22 14:14:57 +0200

    fix x defconfig

--- bin/buildroot.sh
+++ bin/buildroot.sh
@@ -12,21 +12,19 @@ brExternal=$projectDir/br_external
 echo "Directory ${distributionDir} will be mounted to /dist."
 echo "And config dir ${configDir} will be mounted to /configs."
 echo "And br_external dir ${brExternal} will be mounted to /${brExternal}."
-echo "Note, O and BR2_EXTERNAL are already defined in the container and does not need to be exported"
 echo ""
 echo "Load config then run make"
 echo ""
-echo "    make defconfig BR2_DEFCONFIG=/configs/rpi2_x_defconfig"
-echo "    make menuconfig"
-echo "    make savedefconfig BR2_DEFCONFIG=/configs/rpi2_x_defconfig"
-echo "    make"
+echo "    make O=/dist BR2_EXTERNAL=/br_external defconfig BR2_DEFCONFIG=/configs/rpi2_lmn-3_defconfig"
+echo "    make O=/dist BR2_EXTERNAL=/br_external menuconfig"
+echo "    make O=/dist BR2_EXTERNAL=/br_external savedefconfig BR2_DEFCONFIG=/configs/rpi2_lmn-3_defconfig"
+echo "    make O=/dist BR2_EXTERNAL=/br_external"
 
 
+# FORCE_UNSAFE_CONFIGURE allows buildroot to be run as root
 docker run --rm -it \
     -v ./${distributionDir}:/dist \
     -v ./${brExternal}:/br_external  \
     -v ./${configDir}:/configs  \
-    --env O=/dist \
-    --env BR2_EXTERNAL=/br_external/ \
     --env FORCE_UNSAFE_CONFIGURE=1 \
     $baseImageName
